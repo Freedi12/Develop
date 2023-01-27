2fab4b5f1548c1739a3fb125a0e0f3fb3319c37b
Freedi12 <121532009+Freedi12@users.noreply.github.com>
2023-01-27 23:35:31 +0300

    Add files via upload
    vers.4

--- s21vers2.c
+++ s21vers2.c
@@ -27,7 +27,7 @@ struct danno_format znach;
 int main() {
   char *str = NULL;
   // char *str1 = NULL;
-  const char *format = "%c";//+0102.*d
+  const char *format = "%+234.31c%35.24d%*c%%";  //+0102.*d
   char *uno_str = "d";
   int duo_str = 2;
   double trio_str = 1.21;
@@ -41,27 +41,44 @@ int main() {
 }
 
 int s21_sprintf(char *str, const char *format, ...) {
-  int i = 0;
+  int i = 1;
   printf("str%s\n", str);
   // это все в цикле
-  i = pars_format(format, i);
-  str = "lol";
-  va_list predarg;
-  va_start(predarg, format);
+  while (i >= 0) {
+    i = pars_format(format, i);
+    printf("i = %d\n", i);
+    str = "lol";
+    va_list predarg;
+    va_start(predarg, format);
+    // printf("zwezda-2===$$$ %d\n", znach.shirina.zwezda);
+    printf("bukva4  === %c\n", znach.specificator);
+    printf("dlina=1= %ld\n", znach.shirina.col_simvolov);
+
+    // znach.shirina.col_simvolov = 0;
+    printf("zwezda-2===12 %d\n", znach.shirina.zwezda);
+    printf("dlina== %ld\n", znach.shirina.col_simvolov);
+    printf("bukva3  = %c\n", znach.specificator);
+    printf("struct %d\n", znach.flagi.plus);
+  }
   return 1;
 }
 
 int pars_format(const char *format, int i) {
+  printf("zwezda-1===$$$ %d\n", znach.shirina.zwezda);
+  struct danno_format znach = {{0, 0, 0, 0, 0}, {0, 0}, {0, 0}, 0, 0};
+  printf("zwezda-2===$$$ %d\n", znach.shirina.zwezda);
   int wixod = 0;
-  i++;
+  // i++;
+  printf("pizda\n");
   while (/*(format[i] != '%')*/ (wixod == 0) && (format[i] != '\0')) {
+    printf("pizda2\n");
     switch (format[i]) {
       case '-':
         znach.flagi.minus = 1;
         break;
       case '+':
         znach.flagi.plus = 1;
-        printf("struct %d\n", znach.flagi.plus);
+        printf("struct123=== %d\n", znach.flagi.plus);
         break;
       case ' ':
         znach.flagi.space = 1;
@@ -87,6 +104,7 @@ int pars_format(const char *format, int i) {
         break;
       case '*':
         znach.shirina.zwezda = 1;
+        printf("zwezda-2=== %d\n", znach.shirina.zwezda);
         break;
       case '.':
         i++;
@@ -99,6 +117,7 @@ int pars_format(const char *format, int i) {
                 (znach.tochnost.chislo * 10) + (format[i] - '0');
             i++;
           }
+          i--;
         }
         printf("chislo %d\n", znach.tochnost.chislo);
         printf("zwezda %d\n", znach.tochnost.zwezda);
@@ -118,6 +137,7 @@ int pars_format(const char *format, int i) {
         break;
       case 'd':
         znach.specificator = format[i];
+        printf("bukva2 %c\n", znach.specificator);
         break;
       case 'i':
         znach.specificator = format[i];
@@ -161,10 +181,11 @@ int pars_format(const char *format, int i) {
       case '%':
         if (format[i - 1] == '%') {
           znach.specificator = format[i];
-          i++;
+          // i++;
+          printf("i2%d\n", i);
           printf("prozent %c\n", znach.specificator);
-        } 
-          wixod = 1;
+        }
+        wixod = 1;
         // проnисать обнуление переменной колличества символов
         /* code */
         break;
@@ -172,6 +193,9 @@ int pars_format(const char *format, int i) {
         break;
     }
     i++;
+  }
+  if (format[i] == '\0') {
+    i = -1;
   }
   return i;
 }
