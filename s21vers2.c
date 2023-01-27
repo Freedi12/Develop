bf489e425524bf7fa2b81fed631d5c560cb8fe3f
Freedi12 <121532009+Freedi12@users.noreply.github.com>
2023-01-27 20:45:08 +0300

    Add files via upload
    vers.3

--- s21vers2.c
+++ s21vers2.c
@@ -3,19 +3,23 @@
 #include <string.h>
 
 int s21_sprintf(char *str, const char *format, ...);
-void pars_format(const char *format);
+int pars_format(const char *format, int i);
 
 struct danno_format {
   struct {
     int plus, minus, hesh, nool, space;
-  }flagi;
-struct {
-  int zwezda;
-  long int col_simvolov;
-}shirina;
+  } flagi;
+  struct {
+    int zwezda;
+    long int col_simvolov;
+  } shirina;
 
-  int chir_polja;
-  int tochnost;
+  struct {
+    unsigned int chislo;
+    int zwezda;
+  } tochnost;
+  char dlina;
+  char specificator;
 };
 
 struct danno_format znach;
@@ -23,7 +27,7 @@ struct danno_format znach;
 int main() {
   char *str = NULL;
   // char *str1 = NULL;
-  const char *format = "%+0d";
+  const char *format = "%c";//+0102.*d
   char *uno_str = "d";
   int duo_str = 2;
   double trio_str = 1.21;
@@ -37,109 +41,137 @@ int main() {
 }
 
 int s21_sprintf(char *str, const char *format, ...) {
+  int i = 0;
   printf("str%s\n", str);
-  pars_format(format);
-str = "lol";
+  // это все в цикле
+  i = pars_format(format, i);
+  str = "lol";
   va_list predarg;
   va_start(predarg, format);
   return 1;
 }
 
-void pars_format(const char *format) {
-  int i = 0;
-  while (/*(format[i] != '%') && */(format[i] != '\0')) {
+int pars_format(const char *format, int i) {
+  int wixod = 0;
+  i++;
+  while (/*(format[i] != '%')*/ (wixod == 0) && (format[i] != '\0')) {
     switch (format[i]) {
-        case '-':
-          znach.flagi.minus = 1;
-          break;
-        case '+':
-          znach.flagi.plus = 1;
-          printf ("struct %d\n", znach.flagi.plus);
-          break;
-        case ' ':
-          znach.flagi.space = 1;
-          break;
-        case '#':
-          znach.flagi.hesh = 1;
-          break;
-        case '0':
-        if ((format[i-1] >= '0') && (format[i-1] <='9')) {
-          znach.shirina.col_simvolov = (znach.shirina.col_simvolov *10)+(format[i]-'0');
+      case '-':
+        znach.flagi.minus = 1;
+        break;
+      case '+':
+        znach.flagi.plus = 1;
+        printf("struct %d\n", znach.flagi.plus);
+        break;
+      case ' ':
+        znach.flagi.space = 1;
+        break;
+      case '#':
+        znach.flagi.hesh = 1;
+        break;
+      case '0':
+        if ((format[i - 1] >= '0') && (format[i - 1] <= '9')) {
+          znach.shirina.col_simvolov =
+              (znach.shirina.col_simvolov * 10) + (format[i] - '0');
+          printf("dlina2;' %ld\n", znach.shirina.col_simvolov);
+          i++;
         } else {
           znach.flagi.nool = 1;
-          printf ("struct22 %d\n", znach.flagi.nool);
+          printf("struct22 %d\n", znach.flagi.nool);
           break;
         }
+      case '1' ... '9':
+        znach.shirina.col_simvolov =
+            (znach.shirina.col_simvolov * 10) + (format[i] - '0');
+        printf("dlina %ld\n", znach.shirina.col_simvolov);
+        break;
+      case '*':
+        znach.shirina.zwezda = 1;
+        break;
+      case '.':
+        i++;
+        if (format[i] == '*') {
+          znach.tochnost.zwezda = 1;
 
-        case '1' ... '9':
-          /* code */
-          break;
-        case '*':
-          /* code */
-          break;
-        case '.':
-          /* code */
-          break;
-        case 'h':
-          /* code */
-          break;
-        case 'I':
-          /* code */
-          break;
-        case 'L':
-          /* code */
-          break;
-        case 'c':
-          /* code */
-          break;
-        case 'd':
-          /* code */
-          break;
-        case 'i':
-          /* code */
-          break;
-        case 'e':
-          /* code */
-          break;
-        case 'E':
-          /* code */
-          break;
-        case 'f':
-          /* code */
-          break;
-        case 'g':
-          /* code */
-          break;
-        case 'G':
-          /* code */
-          break;
-        case 'o':
-          /* code */
-          break;
-        case 's':
-          /* code */
-          break;
-        case 'u':
-          /* code */
-          break;
-        case 'x':
-          /* code */
-          break;
-        case 'X':
-          /* code */
-          break;
-        case 'p':
-          /* code */
-          break;
-        case 'n':
-          /* code */
-          break;
-        case '%':
-          /* code */
-          break;
-        default:
-          break;
-      }
-      i++;
+        } else {
+          while ((format[i] >= '0') && (format[i] <= '9')) {
+            znach.tochnost.chislo =
+                (znach.tochnost.chislo * 10) + (format[i] - '0');
+            i++;
+          }
+        }
+        printf("chislo %d\n", znach.tochnost.chislo);
+        printf("zwezda %d\n", znach.tochnost.zwezda);
+        break;
+      case 'h':
+        znach.dlina = format[i];
+        break;
+      case 'l':
+        znach.dlina = format[i];
+        break;
+      case 'L':
+        znach.dlina = format[i];
+        break;
+      case 'c':
+        znach.specificator = format[i];
+        printf("bukva %c\n", znach.specificator);
+        break;
+      case 'd':
+        znach.specificator = format[i];
+        break;
+      case 'i':
+        znach.specificator = format[i];
+        break;
+      case 'e':
+        znach.specificator = format[i];
+        break;
+      case 'E':
+        znach.specificator = format[i];
+        break;
+      case 'f':
+        znach.specificator = format[i];
+        break;
+      case 'g':
+        znach.specificator = format[i];
+        break;
+      case 'G':
+        znach.specificator = format[i];
+        break;
+      case 'o':
+        znach.specificator = format[i];
+        break;
+      case 's':
+        znach.specificator = format[i];
+        break;
+      case 'u':
+        znach.specificator = format[i];
+        break;
+      case 'x':
+        znach.specificator = format[i];
+        break;
+      case 'X':
+        znach.specificator = format[i];
+        break;
+      case 'p':
+        znach.specificator = format[i];
+        break;
+      case 'n':
+        znach.specificator = format[i];
+        break;
+      case '%':
+        if (format[i - 1] == '%') {
+          znach.specificator = format[i];
+          i++;
+          printf("prozent %c\n", znach.specificator);
+        } 
+          wixod = 1;
+        // проnисать обнуление переменной колличества символов
+        /* code */
+        break;
+      default:
+        break;
+    }
+    i++;
   }
+  return i;
 }
