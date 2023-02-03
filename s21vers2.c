c3ffb4385c6d83cb1cf20945c3d125e8254e48ca
Freedi12 <121532009+Freedi12@users.noreply.github.com>
2023-02-03 07:31:48 +0300

    молюсь IT-богам пусть все будет хорошо сэтим кодом

--- s21vers2.c
+++ s21vers2.c
@@ -1,3 +1,5 @@
+#include <locale.h>
+#include <math.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +7,9 @@
 
 char s21_sprintf(char *str, const char *format, ...);
 char *swjaz(char n);
+char *l_c_fun(wchar_t lc);
+char *fun_d(int n);
+char *fun_flagi(char *str);
 
 struct danno_format {
   struct {
@@ -26,14 +31,17 @@ struct danno_format {
 struct danno_format znach;
 
 int main() {
+  (void)setlocale(LC_ALL, "");
+
   char str[1024];
   // char str1[1024];
-  const char *format = "%c";  //%-12c%+35.24d%e
-  char uno_str = {'s'};
-  // int duo_str = 2;
+  const char *format = "%lc%-10d";  //%-12c%+35.24d%e
+  // char uno_str = {'s'};
+  wchar_t lc = L'd';
+  int duo_str = -2;
   // double trio_str = 1.21;
 
-  s21_sprintf(str, format,  uno_str/*duo_str,, trio_str*/);
+  s21_sprintf(str, format, lc, duo_str /*uno_str  trio_str*/);
   // sprintf(str1, "%-12c", uno_str /*duo_str, trio_str*/);
   printf("%s\n", str);
   // printf("%s\n", str1);
@@ -63,7 +71,8 @@ char s21_sprintf(char *str, const char *format, ...) {
         if ((format[i - 1] >= '0') && (format[i - 1] <= '9')) {
           znach.shirina.col_simvolov =
               (znach.shirina.col_simvolov * 10) + (format[i] - '0');
-          i++;
+          // i++;
+          break;
         } else {
           znach.flagi.nool = 1;
           break;
@@ -101,6 +110,7 @@ char s21_sprintf(char *str, const char *format, ...) {
         znach.specificator =
             format[i];  // нужно ли заполнение спецификатора !?!?!?!?
         int bol = 0;
+
         if (znach.shirina.zwezda == 1) {
           bol = va_arg(predarg, int);
           if (bol < 0) {
@@ -110,17 +120,25 @@ char s21_sprintf(char *str, const char *format, ...) {
             znach.shirina.col_simvolov = bol;
           }
         }
-        str1 = swjaz(va_arg(predarg, int));
-        // printf("1%s\n", str);
+        if (znach.dlina == 'l') {
+          str1 = l_c_fun(va_arg(predarg, wchar_t));
+        } else {
+          str1 = swjaz(va_arg(predarg, int));
+        }
         strcat(str, str1);
         memset(&znach, 0, sizeof(znach));
         if (str1 != NULL) {
-        free(str1);
-        // printf("cool");
+          free(str1);
         }
         break;
       case 'd':
         znach.specificator = format[i];
+        str1 = fun_d(va_arg(predarg, int));
+        strcat(str, str1);
+        memset(&znach, 0, sizeof(znach));
+        if (str1 != NULL) {
+          free(str1);
+        }
         break;
       case 'i':
         znach.specificator = format[i];
@@ -165,7 +183,6 @@ char s21_sprintf(char *str, const char *format, ...) {
         if (format[i - 1] == '%') {
           znach.specificator = format[i];
         }
-
         break;
       default:
         break;
@@ -177,6 +194,120 @@ char s21_sprintf(char *str, const char *format, ...) {
   }
   va_end(predarg);
   return *str;
+}
+
+char *fun_d(int n) {
+  char *str_i = '\0';
+  int len = 0, buf_n = 0, buf = 0;
+
+  str_i = (char *)malloc(12 * sizeof(char));
+
+  if (n > 0) {
+    buf_n = n;
+    while (buf_n > 9) {
+      len++;
+      buf_n /= 10;
+    }
+    buf_n = n;
+    for (int a = 0; len >= 0; a++) {
+      if (znach.flagi.plus == 1) {
+        str_i[0] = '+';
+        znach.shirina.col_simvolov--;
+        a++;
+      }
+      if (znach.flagi.space == 1) {
+        str_i[0] = ' ';
+        znach.shirina.col_simvolov--;
+        a++;
+      }
+      buf = pow(10, len);
+      str_i[a] = (buf_n / buf) + '0';
+      buf_n = buf_n - ((buf_n / buf) * buf);
+      len--;
+    }
+  } else {
+    // if (znach.flagi.minus == 1) {
+    znach.shirina.col_simvolov--;
+    // }
+    buf_n = n * (-1);
+    while (buf_n > 9) {
+      len++;
+      buf_n /= 10;
+    }
+    buf_n = n * (-1);
+    str_i[0] = '-';
+    for (int a = 1; len >= 0; a++) {
+      buf = pow(10, len);
+      str_i[a] = (buf_n / buf) + '0';
+      buf_n = buf_n - ((buf_n / buf) * buf);
+      len--;
+    }
+  }
+  if (znach.shirina.col_simvolov > 0) {
+    str_i = fun_flagi(str_i);
+  }
+  return str_i;
+}
+
+char *fun_flagi(char *str) {
+  char *str2 = "w";
+  char *str3 = "";
+  int i = 0;
+
+  str3 = (char *)malloc(1024 * sizeof(char));
+
+  while (str[i] != '\0') {
+    str3[i] = str[i];
+    i++;
+  }
+  str2 = (char *)malloc(sizeof(znach.shirina.col_simvolov * 2));
+  for (int i = 0; i < znach.shirina.col_simvolov - 1; i++) {
+    str2[i] = ' ';
+  }
+  if (znach.flagi.minus == 0) {
+    strcat(str2, str3);
+    free(str3);
+    return str2;
+  } else {
+    strcat(str3, str2);
+    free(str2);
+    return str3;
+  }
+}
+
+char *l_c_fun(wchar_t lc) {
+  char *pri = "q";
+  char *pri2 = "w";
+  int n = 10;
+  wchar_t e_lc = '\0';
+  wchar_t m_ls[3];
+
+  if (znach.shirina.col_simvolov > 0) {
+    n = znach.shirina.col_simvolov * 2;
+  }
+  m_ls[0] = lc;
+  m_ls[1] = e_lc;
+
+  pri = (char *)malloc(n * MB_CUR_MAX + 1);
+  wcstombs(pri, m_ls, n);
+
+  if (znach.shirina.col_simvolov > 0) {
+    pri2 = (char *)malloc(sizeof(znach.shirina.col_simvolov * 2));
+    for (int i = 0; i < znach.shirina.col_simvolov - 1; i++) {
+      pri2[i] = ' ';
+    }
+    if (znach.flagi.minus == 0) {
+      strcat(pri2, pri);
+      free(pri);
+      return pri2;
+    } else {
+      strcat(pri, pri2);
+      free(pri2);
+      return pri;
+    }
+  } else {
+    return pri;
+  }
 }
 
 char *swjaz(char dev) {
@@ -192,10 +323,10 @@ char *swjaz(char dev) {
         pri[i] = ' ';
       }
     } else {
-      for (int i = 0; i < znach.shirina.col_simvolov-1; i++) {
+      for (int i = 0; i < znach.shirina.col_simvolov - 1; i++) {
         pri[i] = ' ';
       }
-      pri[znach.shirina.col_simvolov-1] = dev;
+      pri[znach.shirina.col_simvolov - 1] = dev;
     }
   }
   // printf("1=/=/==/=/ %c\n", dev);
--- sprintf.c
+++ sprintf.c
@@ -5,11 +5,12 @@
 
 int main() {
   char str1[1024];
-  const char *format = "%lc";  //%-12c%+35.24d%e
-  char uno_str = {'s'};
-//   int duo_str = 2;
+  const char *format = "%lc%-10d";  //%-12c%+35.24d%e
+  // char uno_str = {'s'};
+  wchar_t lc = L'd';
+  int duo_str = -2;
   // double trio_str = 1.21;
 
-  sprintf(str1, format, uno_str/* duo_str,, trio_str*/);
+  sprintf(str1, format,lc,  duo_str /*uno_str duo_str,, trio_str*/);
   printf("%s\n", str1);
 }
